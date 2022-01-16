b6e25d397375d23d077676f5b7b8fc9d97f6a509
Shaun Kent <shaunkent81@gmail.com>
2022-01-15 18:44:15 -0800

    inserted content into header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <React.Fragment>
+      <h1>React Tap</h1>
+      <h3>Your favorite coding themed bar!</h3>
+    </React.Fragment>
+  )
+}
+
+export default Header
