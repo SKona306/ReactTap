a099fd18a16af73273a4be56d95607876e6cdea5
Shaun Kent <shaunkent81@gmail.com>
2022-01-15 19:39:53 -0800

    added form in newKegForm component and created function to handle submission

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import { v4 } from 'uuid';
 
 function NewKegForm(props) {
+
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    console.log(event.target.names.value);
+    console.log(event.target.brand.value);
+    console.log(event.target.price.value);
+    console.log(event.target.alcpercent.value);
+    console.log(event.target.pints.value);
+  }
+
   return (
     <React.Fragment>
-      <h1>this is a form</h1>
+      <form onSubmit={handleNewKegFormSubmission}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name" />
+        <input
+          type="text"
+          name="brand"
+          placeholder="Brand" />
+        <input
+          type="number"
+          min={1}
+          name="price"
+          placeholder="Price" />
+        <input
+          type="number"
+          name="alcpercent"
+          placeholder="Alcohol Percentage" />
+        <input 
+          type="number"
+          min={1}
+          name="pints"
+          placeholder="Pints in stock" />
+      </form>
     </React.Fragment>
   )
 }
