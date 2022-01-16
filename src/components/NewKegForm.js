b5fcb706de5839c393e2da863eea534166e46b87
Shaun Kent <shaunkent81@gmail.com>
2022-01-15 19:20:41 -0800

    created newkegform and added placeholder content

---
+++ src/components/NewKegForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NewKegForm(props) {
+  return (
+    <React.Fragment>
+      <h1>this is a form</h1>
+    </React.Fragment>
+  )
+}
+
+export default NewKegForm
