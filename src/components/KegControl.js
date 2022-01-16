58b0fe55ab002bf526f399e9f3986c880078d5ac
Shaun Kent <shaunkent81@gmail.com>
2022-01-15 21:31:33 -0800

    added mainKegList and selectedKeg to state and changed conditional rendering logic to match

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NewKegForm from './NewKegForm';
 import KegList from './KegList';
+import KegDetail from './KegDetail';
 
 class KegControl extends React.Component {
 
@@ -8,7 +9,8 @@ class KegControl extends React.Component {
     super(props);
     this.state = {
       formVisibleOnPage: false,
-      mainKegList: []
+      mainKegList: [],
+      selectedKeg: null
     };
   }
 
@@ -26,14 +28,22 @@ class KegControl extends React.Component {
     });
   }
 
+  handleChangingSelectedKeg = (id) => {
+    const selectedKeg =this.state.mainKegList.filter(keg => keg.id === id)[0];
+    this.setState({selectedKeg: selectedKeg});
+  }
+
   render() {
     let currentlyVisibleState = null;
     let buttonText = null;
-    if(this.state.formVisibleOnPage) {
+    if(this.state.selectedKeg != null) {
+      currentlyVisibleState = <KegDetail keg={this.state.selectedKeg}/>
+      buttonText= "Return to Keg List";
+    } else if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewKegForm onNewKegCreation={this.handleAddingNewKegToList}/>
       buttonText = "Return to Keg List";
     } else {
-      currentlyVisibleState = <KegList kegList={this.state.mainKegList} />
+      currentlyVisibleState = <KegList kegList={this.state.mainKegList} onKegSelection = {this.handleChangingSelectedKeg}/>
       buttonText = "Add Keg";
     }
     return(
