abf56a86f062ecc1ec536ba08558fca34e7f7895
Shaun Kent <shaunkent81@gmail.com>
2022-01-15 22:56:02 -0800

    add button to buy keg, add tenrary statment to manage out of stock and buy button hide

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -2,8 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 function KegDetail(props) {
-  const { keg, onClickingDelete, onClickingIncrease } = props;
-
+  const { keg, onClickingDelete, onClickingIncrease, onClickingDecrease } = props;
+  const pints = keg.pints === 0 ? "Out of Stock" : keg.pints;
+  const buyButton = keg.pints === 0 ? <p></p> : <button onClick={() => onClickingDecrease(keg.id)}>Buy</button>
   return (
     <React.Fragment>
       <h1>Keg Details:</h1>
@@ -13,10 +14,11 @@ function KegDetail(props) {
       <hr />
       <p>Alcohol Content: {keg.alcpercent}%</p>
       <p>Price: ${keg.price}</p>
-      <p>Pints in stock: {keg.pints}</p>
-      <button onClick={() => onClickingIncrease(keg.id)}>Restock</button>
+      <p>Pints in stock: {pints}</p>
       <button onClick={props.onClickingEdit}>Update Keg</button>
       <button onClick={() => onClickingDelete(keg.id)}>Delete Keg</button>
+      <button onClick={() => onClickingIncrease(keg.id)}>Restock</button>
+      {buyButton}
     </React.Fragment>
   );
 }
