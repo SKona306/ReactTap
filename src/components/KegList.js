bce07a4895a3e357913b8517c7d55feac5b2ed26
Shaun Kent <shaunkent81@gmail.com>
2022-01-15 19:09:11 -0800

    add looping to keglist component

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import Keg from './Keg';
 
+const mainKegList = [
+  {
+    name: "beerOne",
+    brand: "superBeer",
+    price:"10",
+    alcpercent: "15",
+    pints:"25"
+  },
+  {
+    name: "beerTwo",
+    brand: "electroBeer",
+    price: "12",
+    alcpercent: "12",
+    pints: "30"
+  }
+]
+
 function KegList() {
   return (
     <React.Fragment>
-      <Keg 
-      name='beer'
-      brand='superBeer'
-      price='10'
-      alcpercent='15'
-      pints='25'/>
+      <hr />
+      {mainKegList.map((keg, index) => 
+        <Keg 
+          name={keg.name}
+          brand={keg.brand}
+          price={keg.price}
+          alcpercent={keg.alcpercent}
+          pints={keg.pints}
+          key={index}/>
+      )}
     </React.Fragment>
   )
 }
