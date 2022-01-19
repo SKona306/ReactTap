a7236da446985623c3699f570e060ed721b27b26
Shaun Kent <shaunkent81@gmail.com>
2022-01-18 20:07:41 -0800

    create keglistreducer and added two switch cases

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,22 @@
 export default (state = {}, action) => {
-  return state;
-}
+  const { name, brand, alcpercent, price, pints, id } = action;
+  switch(action.type) {
+    case 'ADD_KEG' : 
+      return Object.assign({}, state, {
+        [id] : {
+          name: name,
+          brand: brand,
+          alcpercent: alcpercent,
+          price: price,
+          pints: pints,
+          id: id
+        }
+      });
+    case 'DELETE_KEG': 
+      let newState = {...state};
+      delete newState[id];
+      return newState;
+    default :
+    return state;
+  }
+};
