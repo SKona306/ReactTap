5e6bf98a3f9e00afca36417fec2c4763265bce6d
Shaun Kent <shaunkent81@gmail.com>
2022-01-18 20:19:06 -0800

    connect kegControl component to redux store

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -3,6 +3,7 @@ import NewKegForm from './NewKegForm';
 import KegList from './KegList';
 import KegDetail from './KegDetail';
 import EditKegForm from './EditKegForm';
+import { connect } from 'react-redux';
 
 class KegControl extends React.Component {
 
@@ -130,5 +131,7 @@ class KegControl extends React.Component {
     );
   }
 }
+
+KegControl = connect()(KegControl);
 
 export default KegControl;
