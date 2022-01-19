a981bef3679c7f0145fecf92218faaebeb83a296
Shaun Kent <shaunkent81@gmail.com>
2022-01-18 20:08:41 -0800

    added two new tests to check the functionality of add and delete in keglistreducer

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,9 +1,78 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+
+  const currentState = {
+    1: {
+      name: 'React Brew',
+      brand: 'Reactify',
+      alcpercent: '12',
+      price: '17',
+      pints: 124,
+      id: 1
+    },
+    2: {
+      name: 'JerrysBrew',
+      brand: 'Jerry',
+      alcpercent: '9',
+      price: '10',
+      pints: 124,
+      id: 2
+    }
+  }
+
+  let action;
+  let kegData = {
+    name: 'React Brew',
+    brand: 'Reactify',
+    alcpercent: '7',
+    price: '17',
+    pints: 124,
+    id: 1
+  }
   
   test('Should return default state if no action is recognized', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
+
+  test('Should successfully add new kegData to the mainKegList', () => {
+    const { name, brand, alcpercent, price, pints, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      alcpercent: alcpercent,
+      price: price,
+      pints: pints,
+      id: id
+    }
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brand: brand,
+        alcpercent: alcpercent,
+        price: price,
+        pints: pints,
+        id: id
+      }
+    });
+  });
+
+  test('Should successfully delete a keg', () => {
+    action = {
+      type: 'DELETE_KEG',
+      id: 1
+    }
+    expect(kegListReducer(currentState, action)).toEqual({
+      2: {
+        name: 'JerrysBrew',
+        brand: 'Jerry',
+        alcpercent: '9',
+        price: '10',
+        pints: 124,
+        id: 2
+      }
+    })
+  })
 
 });
