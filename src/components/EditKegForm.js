66c2437478d010c64711a9fb620fab8faa76298c
Shaun Kent <shaunkent81@gmail.com>
2022-01-18 17:48:55 -0800

    change value type of pints on edit form from string to number

--- src/components/EditKegForm.js
+++ src/components/EditKegForm.js
@@ -7,7 +7,7 @@ function EditKegForm(props) {
 
   function handleEditKegFormSubmission(event) {
     event.preventDefault();
-    props.onEditKeg({name: event.target.name.value, brand: event.target.brand.value, price: event.target.price.value, alcpercent: event.target.alcpercent.value, pints: event.target.pints.value, id: keg.id})
+    props.onEditKeg({name: event.target.name.value, brand: event.target.brand.value, price: event.target.price.value, alcpercent: event.target.alcpercent.value, pints: parseInt(event.target.pints.value), id: keg.id})
   }
   return (
     <React.Fragment>
